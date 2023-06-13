1923e404ed87de9dc08ad2e869c1997bdf720c08
unknown <waqasarif588@gmail.com>
2023-06-13 20:46:14 +0500

    office 13

---
+++ controllers/categoryController.js
@@ -0,0 +1,20 @@
+import categoryModel from "../models/categoryModel.js";
+class categoryController{
+     static categories = (req,res) => {
+
+    }
+    static addCategory = async(req,res) => {
+
+    }
+    static editCategory = async(req,res) => {
+
+    }
+    static updateCategory = async(req,res) => {
+
+    }
+    static deleteCategory = async(req,res) => {
+        
+    }
+}
+
+export default categoryController;
---
+++ controllers/productController.js
@@ -0,0 +1,19 @@
+import productModel from "../models/productModel.js";
+class productController{
+    static products = (req,res) => {
+
+    }
+    static addProduct = async(req,res) => {
+
+    }
+    static editProduct = async(req,res) => {
+
+    }
+    static updateProduct = async(req,res) => {
+
+    }
+    static deleteProduct = async(req,res) => {
+        
+    }
+}
+export default productController;
